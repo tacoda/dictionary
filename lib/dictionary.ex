6d8783abd722354a73d5ca570ece1a04df5bd0be
Ian Johnson <ijohnson@covermymeds.com>
2019-05-21 01:12:08 -0400

    Initial commit

---
+++ lib/dictionary.ex
@@ -0,0 +1,24 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for Dictionary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dictionary.hello()
+      :world
+
+  """
+
+  def random_word() do
+    Enum.random(word_list())
+  end
+
+  def word_list() do
+    contents = File.read! "assets/words.txt"
+    list = String.split contents, ~r/\n/
+  end
+end
