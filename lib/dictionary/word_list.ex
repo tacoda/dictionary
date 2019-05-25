66a784af5a30b14a82d32eb61aec5075ac2fb120
Ian Johnson <ijohnson@covermymeds.com>
2019-05-25 11:31:55 -0400

    Add supervisor

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,12 +1,15 @@
 defmodule Dictionary.WordList do
 
   @me __MODULE__
-  
+
   def start_link() do
     Agent.start_link(&word_list/0, name: @me)
   end
 
   def random_word() do
+    # if :rand.uniform < 0.33 do
+    #   Agent.get(@me, fn _ -> exit(:boom) end)
+    # end
     Agent.get(@me, &Enum.random/1)
   end
 
